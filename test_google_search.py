8a248a17439d312e4b08d3237456d922742610de
Pavel Sazonov <pavelsazonov2015@gmail.com>
2019-05-27 19:53:02 +0300

    End allure is working

--- test_google_search.py
+++ test_google_search.py
@@ -1,2 +1,19 @@
+from selenium.webdriver.chrome.webdriver import WebDriver
+from selenium.webdriver.support.wait import WebDriverWait
+import allure
+
+
+@allure.title('Результатов поиска больше 5')
+@allure.severity('Severity.BLOCKER')
 def test_google_search():
-    ...
+    driver = WebDriver(executable_path='D://selenium//chromedriver.exe')
+    with allure.step('Ожидаем страницу поиска'):
+        driver.get('https://google.com.ua')
+
+    with allure.step('Ищем https://www.python.org/'):
+        search_input = driver.find_element_by_xpath('//input [@name="q"]')
+        search_button = driver.find_element_by_xpath('//input [@name="btnK"]')
+        search_input.send_keys('https://www.python.org/')
+        driver.implicitly_wait(30)
+        search_button.click()
+
